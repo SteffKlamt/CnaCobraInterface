9e9735a0e4cbfb4662536bb81a5bdd06812bc2c5
Steffen Klamt <klamt@pc977.mpi-magdeburg.mpg.de>
2017-12-18 16:34:14 +0100

    Original CNA-COBRA conversion functions

---
+++ CNAcna2cobra.m
@@ -0,0 +1,58 @@
+ 
+function cbmodel= CNAcna2cobra(cnap)
+% CellNetAnalyzer API function 'CNAcna2cobra'
+%
+% Usage:  cbmodel = CNAcna2cobra(cnap)
+%
+% Input: cnap is a CNA mass-flow project structure
+%
+% Output: cbmodel is a COBRA Toolbox model.
+%
+% Creates a COBRA Model from a CNA project.
+
+
+% create new CNA Project with default marcomolecule proportions
+cnap2= CNAgetMFNetwork(cnap);
+
+cbmodel = struct();
+cbmodel.description = 'COBRA model converted from a CNA project';
+cbmodel.rxns = cellstr(cnap2.reacID);
+cbmodel.mets = cellstr(cnap2.specID); 
+cbmodel.S =  cnap2.stoichMat;
+cbmodel.lb =  cnap2.reacMin;
+cbmodel.ub =  cnap2.reacMax; 
+cbmodel.rev = double(cnap2.reacMin<0); % Reversibility vector            
+cbmodel.c =   -cnap2.objFunc; % Objective coefficients; CNA minimizes COBRA maximizes!       
+cbmodel.rxnNames = cellstr(cnap2.reacID);
+cbmodel.metNames = cellstr(cnap2.specLongName); 
+
+% optional fields of COBRA model
+%  cbmodel.subSystems =      'Subsystem name for each reaction (opt)  ';
+%  cbmodel.grRules =         'Gene-reaction association rule for each reaction (opt) ';
+%  cbmodel.rules =           'Gene-reaction association rule in computable form (opt)';
+%  cbmodel.rxnGeneMat =      'Reaction-to-gene mapping in sparse matrix form (opt)   ';
+%  cbmodel.genes =           'List of all genes (opt)      ';
+%  cbmodel.metFormulas =     'Metabolite chemical formula (opt)';
+
+% find indices of external metabolites
+indices = find(cnap2.specExternal);
+disp(['Removing ',num2str(numel(indices)),' external metabolites']);
+
+% remove the external metabolite rows from stoichiometry matrix
+cbmodel.S(indices,:)=[];
+
+% create sparse matrix
+cbmodel.S =  sparse(cbmodel.S);
+
+% remove the external metabolite ids/Names
+cbmodel.mets(indices,:)=[];
+cbmodel.metNames(indices,:)=[];
+cbmodel.b=zeros(size(cbmodel.S,1),1);
+
+% reversible if lower bound < 0
+for n = 1:numel(cbmodel.lb)
+  cbmodel.rev(n) = cbmodel.lb(n)<0;
+end
+
+
+
---
+++ CNAcobra2cna.m
@@ -0,0 +1,42 @@
+ 
+function cnap= CNAcobra2cna(cbmodel)
+% CellNetAnalyzer API function 'CNAcobra2cna'
+%
+% Usage:  cnap = CNAcobra2cna(cbmodel)
+%
+% Input:  cbmodel is a COBRA Toolbox model.
+%
+% Output: cnap is a CNA mass-flow project structure
+%
+% Creates a CNA project from a COBRA model.
+
+
+cnap= struct();
+
+cnap.specID = [char(cbmodel.mets{:})];
+cnap.specLongName = [char(cbmodel.metNames{:})];
+cnap.stoichMat = full(cbmodel.S);
+cnap.reacMin = cbmodel.lb;
+cnap.reacMax = cbmodel.ub;
+cnap.reacID = [char(cbmodel.rxns{:})];
+cnap.objFunc = -cbmodel.c; %%CNA minimizes!
+
+for i=1:size(cnap.specLongName,2)
+	zw=deblank(cnap.specLongName(i,:));
+	if(numel(findstr(' ',zw)))
+		 newstr= strrep(zw,' ','_');
+		 disp(['Warning: Metabolite name with whitespace. Replacing ''',zw,''' with ''',newstr,'''.']);
+		 cnap.specLongName(i,1:numel(zw))=newstr;
+	end
+end
+
+cnap = CNAgenerateMFNetwork(cnap);
+
+if(any(cbmodel.b))
+	disp(' ');
+	disp('Warning: the b-vector of the COBRA model is non-zero. Will translate it as zero-vector (i.e., as homogenoeous system). Use reacMin and reacMax to set inhomogeneous constraints!');
+end
+
+
+
+
